45bbb5441e5d9169e3e3a21753585de2e4b95d82
Xenotime <161914158+xnotime@users.noreply.github.com>
2025-01-26 14:40:56 -0600

    ok file server worksies

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,8 @@
 module github.com/xnotime/lithia/backend
 
 go 1.23.5
+
+require (
+	github.com/alexandrevicenzi/go-sse v1.6.0 // indirect
+	github.com/go-chi/chi/v5 v5.2.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/alexandrevicenzi/go-sse"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const PORT = 11814
 
 func main() {
-	fmt.Println("tacos")
+	// Set up router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	// Set up SSE agent
+	sse_agent := sse.NewServer(nil)
+	defer sse_agent.Shutdown()
+	// Set up file server
+	fs := http.FileServer(http.Dir("./static"))
+	// Set up routes
+	r.Handle("/static/*", http.StripPrefix("/static/", fs))
+	// Listen & serve
+	fmt.Printf("Listening on port %d...\n", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), r)
 }
